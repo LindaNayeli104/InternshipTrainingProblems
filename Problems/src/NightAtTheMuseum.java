2d30588363dc5834f0ef2afbeb5fb82b2a30554a
LindaNayeli104 <linda_nayeli104@hotmail.com>
2020-05-16 22:59:33 -0500

    One problem doesnt work

---
+++ Problems/src/NightAtTheMuseum.java
@@ -0,0 +1,27 @@
+//Problem
+// http://codeforces.com/contest/731/problem/A 
+
+import java.util.Scanner;
+
+public class NightAtTheMuseum {
+    	
+	public static int totalRot(String name) {
+		String nameUp = name.toUpperCase();
+		
+		
+	}
+	
+	
+	public static void main(String args[]){
+        Scanner res = new Scanner(System.in);
+ 
+        String name = res.next();
+        
+        System.out.println(totalRot(name));
+        res.close();
+    }
+    
+    	
+}
+
+//Linda Abundis
