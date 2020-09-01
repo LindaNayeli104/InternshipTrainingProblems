234e015f5a455d71cfc1c9eb9fe652009f8fac68
LindaNayeli104 <linda_nayeli104@hotmail.com>
2020-09-01 11:08:59 -0500

    added 'Cracking the Coding Interview' ploblems section

---
+++ CrackingTheCodingInterview/IsUnique.java
@@ -0,0 +1,28 @@
+//Arrays and Strings
+//Cracking the Coding Interview
+
+public class IsUnique {
+	public static boolean checkChars(String str) {
+		if (str.length() > 128) {
+			return false;
+        }
+        
+		boolean[] chars = new boolean[128];
+		for (int i = 0; i < str.length(); i++) {
+			int charVal = str.charAt(i);
+			if (chars[charVal]) return false;
+			chars[charVal] = true;
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		String[] strs = {"dad", "Dad", "linda", "aPple"};
+		for (String str : strs) {
+			System.out.println(str + "= " + checkChars(str));
+		}
+	}
+
+}
+
+//Linda Abundis
--- Problems_Java/src/MergeTwoSortedLists.java
+++ Problems_Java/src/MergeTwoSortedLists.java
@@ -1,4 +1,39 @@
+//Problem
+//https://leetcode.com/problems/merge-two-sorted-lists/submissions/
 
-public class MergeTwoSortedLists {
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+import java.util.*;
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode current = new ListNode(-1);
+        ListNode head = current;
+        
+       while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                current.next = l1;
+                l1 = l1.next;
+            }else{
+                current.next = l2;
+                l2 = l2.next;
+            }
+           current = current.next;
+        }
+        if(l1 == null){
+            current.next = l2;
+        }else{
+            current.next = l1;
+        }
+        return head.next;
+    }
 }
+
+//Linda Abundis
