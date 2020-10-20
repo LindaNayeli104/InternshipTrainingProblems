1eb7048906ff28690a83378d690f5ef36c1dde31
LindaNayeli104 <linda_nayeli104@hotmail.com>
2020-10-20 12:02:37 -0500

    finally solved this problem, took me some time but finally done

---
+++ TopInterviewQuestions/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,33 @@
+//https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/564/
+class BestTimeToBuyAndSellStockII {
+    public int maxProfit(int[] prices) {
+        if(prices.length <2){
+            return 0;
+        }
+        int min = prices[0];
+        int max = 0;
+        int profit = 0;
+        
+        for(int i=0; i<prices.length;i++){
+            if(prices[i] < min){
+                if(max != 0){
+                    profit += (max-min);
+                }
+                min = prices[i];
+                max = 0;
+            }else{
+                if(prices[i] > max){
+                    max = prices[i];
+                }else{
+                    profit += (max-min);
+                    min = prices[i];
+                    max = 0;
+                }
+            }
+        }
+        if(max != 0){
+            profit += max-min;
+        }
+        return profit;
+    }
+}
